628cbb6132bd953b0cfd46bd24b481e0352aa11e
NoobPro004 <himanshugarg738@gmail.com>
2021-10-25 23:50:17 +0530

    added upto Undo and Redo

--- canvas.js
+++ canvas.js
@@ -6,12 +6,18 @@ canvas.height=window.innerHeight;
 let pencilColor=document.querySelectorAll(".pencil-color");
 let pencilWidthElem=document.querySelector(".pencil-width");
 let eraserWidthElem=document.querySelector(".eraser-width");
+let download=document.querySelector(".download");
+let redo=document.querySelector(".redo");
+let undo=document.querySelector(".undo");
 
 
 let penColor="red";
 let eraserColor="white";
 let penWidth=pencilWidthElem.value;
 let eraserWidth=eraserWidthElem.value;
+
+let undroRedoTracker=[]; // data
+let track=0;  // represents which action from tracker array
 
 
 let mouseDown=false;
@@ -46,14 +52,54 @@ canvas.addEventListener("mousemove",(e)=>{
     if(mouseDown){
         drawStroke({
             x:e.clientX,
-            y:e.clientY
+            y:e.clientY,
+            color : eraserFlag ? eraserColor : penColor,
+            width : eraserWidth ? eraserWidth : penWidth,
         })
     }
 })
 
 canvas.addEventListener("mouseup",(e)=>{
     mouseDown=false;
+
+    let url=canvas.toDataURL();
+    undroRedoTracker.push(url);
+    track=undroRedoTracker.length-1;
+
 })
+
+undo.addEventListener("click",(e)=>{
+    if(track>0)track--;
+    // action 
+    let trackObj={
+        trackValue:track,
+        undroRedoTracker
+    }
+    undoredoCanvas(trackObj);
+
+})
+
+redo.addEventListener("click",(e)=>{
+    if(track<undroRedoTracker.length-1)track++;
+    let trackObj={
+        trackValue:track,
+        undroRedoTracker
+    }
+    undoredoCanvas(trackObj);
+})
+
+function undoredoCanvas(trackObj){
+    track=trackObj.trackValue;
+    undroRedoTracker=trackObj.undroRedoTracker;
+
+    let url=undroRedoTracker[track];
+    let img=new Image();  // new Image refernce element
+    img.src=url;
+    img.onload =(e)=>{
+        tool.drawImage(img,0,0,canvas.width,canvas.height);
+    }
+}
+
 
 function beginPath(strokeObj){
     tool.beginPath();
@@ -62,6 +108,8 @@ function beginPath(strokeObj){
 
 
 function drawStroke(strokeObj){
+    tool.strokeStyle=strokeObj.color;
+    tool.linewidth=strokeObj.width;
     tool.lineTo(strokeObj.x,strokeObj.y);
     tool.stroke();
 }
@@ -92,4 +140,14 @@ eraser.addEventListener("click",(e)=>{
         tool.strokeStyle=penColor;
         tool.linewidth=penWidth;
     }
+})
+
+
+download.addEventListener("click",(e)=>{
+    let url=canvas.toDataURL();
+
+    let a=document.createElement("a");
+    a.href=url;
+    a.download="board.jpg";
+    a.click();
 })
